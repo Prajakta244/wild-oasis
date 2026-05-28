0273b0c530b05bcdaadbc823d4853926d6245a7f
Prajakta244 <58784411+Prajakta244@users.noreply.github.com>
2026-05-28 20:27:42 +0530

    Refactor BookingTable component with useBookings
    Refactor BookingTable to use useBookings hook and add loading state with Spinner.

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -1,10 +1,15 @@
+/* eslint-disable no-unused-vars */
 import BookingRow from "./BookingRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import useBookings from "./useBookings";
+import Spinner from "../../ui/Spinner";
+import Pagination from "../../ui/Pagination";
 
-function BookingTable() {
-  const bookings = [];
-
+const BookingTable = () => {
+  const {bookings,isLoading} = useBookings()
+  console.log(bookings)
+  if(isLoading) return <Spinner/>
   return (
     <Menus>
       <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
@@ -23,6 +28,9 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+        <Table.Footer>
+          <Pagination count={bookings.length+20}/>
+        </Table.Footer>
       </Table>
     </Menus>
   );
