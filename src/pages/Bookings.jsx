df3324b7cab517953be84402520f26010e767706
Prajakta244 <58784411+Prajakta244@users.noreply.github.com>
2026-05-28 20:36:52 +0530

    Add files via upload

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,12 +1,18 @@
+import BookingTable from "../features/bookings/BookingTable";
+import BookingTableOperations from "../features/bookings/BookingTableOperations";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 const Bookings = ()=> {
   return (
-    <Row type="horizontal">
+    <><Row type="horizontal">
       <Heading as="h1">All bookings</Heading>
-      <p>TEST</p>
+      <BookingTableOperations/>
+      </Row>
+      <Row type="vertical">
+      <BookingTable/>
     </Row>
+    </>
   );
 }
 
